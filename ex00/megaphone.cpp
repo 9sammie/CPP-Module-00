ded52a05e1df161df7815f1410c43624d5782167
9sammie <ballet.mathis@gmail.com>
2025-12-02 16:47:17 +0100

    suppressed a.out

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -6,28 +6,27 @@
 /*   By: maballet <maballet@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/24 15:02:42 by maballet          #+#    #+#             */
-/*   Updated: 2025/11/24 19:28:09 by maballet         ###   ########lyon.fr   */
+/*   Updated: 2025/11/26 13:58:11 by maballet         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include<iostream>
-// #include<cctype>
-
-// void	*megaphone(char *str)
-// {
-
-// }
+#include<cctype>
 
 int	main(int argc, char **argv)
 {
-	if (argc != 2) {
+	if (argc < 2) {
 		if (argc == 1)
 			std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
 		return (0);
 	}
-	std::string str = argv[1];
-	for (int i = 0; i <= str.length(); i++)
-		str[i] = std::toupper(str[i]);
-	std::cout << str << std::endl;
+	for (int i = 1; (argv[i]); i++)
+	{
+		std::string str = argv[i];
+		for (size_t curr_char = 0; curr_char < str.length(); curr_char++)
+			str[curr_char] = std::toupper(static_cast<unsigned char>(str[curr_char]));
+		std::cout << str;
+	}
+	std::cout << std:: endl;
 	return (0);
 }
